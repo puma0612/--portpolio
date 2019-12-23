cf0c8bc35ea79338728d5822416b74d4dcf4eb9e
puma0612 <puma303@naver.com>
2019-12-23 16:37:13 +0900

    Revert "Initial commit"
    This reverts commit 434f80683115256ff74a0fb9904fd4296fd7d02d.

--- 개인포트폴리오1/js/intro.js
+++
@@ -1,6 +0,0 @@
-$(function(){
-	$(".section .page.index span.btn").click(function(){
-		$(".section .page.index .intro_dim").fadeOut(300);
-		$(".section .page.index .dim").fadeIn(600);
-	});
-});
--- 개인포트폴리오2/js/main_ie8.js
+++
@@ -1,7 +0,0 @@
-$(function(){
-	// 1) LNB 관련
-	$(".west .nav").css({opacity:0});
-
-	// 2) 그리드 관련
-	$(".grid_wrap span").show().css({opacity:0.08});
-});
